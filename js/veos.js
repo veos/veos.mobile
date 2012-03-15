2480818e5ee09cf7706699b4a68e72f221d43e98
Matt Zukowski <matt@zukowski.ca>
2012-03-15 17:09:16 -0400

    added started js files

---
+++ js/veos.js
@@ -0,0 +1,14 @@
+/*jshint browser: true, devel: true */
+
+window.veos = (function(veos) {
+    var self = veos;
+
+    /* put your functions and stuff here */
+
+    // for example:
+    self.someFunction = function (foo) {
+        console.log("stuff");
+    };
+
+    return self;
+})(window.veos || {});
---
+++ js/veos.model.js
@@ -0,0 +1,15 @@
+/*jshint browser: true, devel: true */
+
+window.veos = (function(veos) {
+    var self = {};
+
+    /* put your functions and stuff here */
+
+    // for example:
+    self.someFunction = function (foo) {
+        console.log("stuff");
+    };
+
+    veos.model = self;
+    return veos;
+})(window.veos || {});
