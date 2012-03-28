2807b5afb85e37e493ae19feb657be54c34f9e51
Matt Zukowski <matt@zukowski.ca>
2012-03-28 11:57:16 -0400

    moved to phonegap (cordova) 1.5.0

--- js/report.js
+++ js/report.js
@@ -4,14 +4,43 @@
 window.report = (function(report) {
     var self = report;
 
+    // Called when a photo is successfully retrieved
+    function cameraSuccess (imageURI) {
+      console.log("Got image: ", imageURI);
+
+      // Get image handle
+      var firstImage = document.getElementById('photo-preview');
+
+
+      // Unhide image elements
+      //smallImage.style.display = 'block';
+
+      // Show the captured photo
+      // The inline CSS rules are used to resize the image
+      //
+      //firstImage.src = "data:image/jpeg;base64," + imageData;
+      firstImage.src = imageURI;
+    };
+
+    function cameraError (message) {
+      //alert('Failed because: ' + message);
+      console.warn('Photo capture failed.');
+    };
+
+    
+
     // Functions for report.html
     // take a picture
     self.capturePhoto = function () {
-        console.log('before camera call');
-        // Take picture using device camera and retrieve image as base64-encoded string
-        navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
-          quality: 50
-        });
+      var options = {
+        quality: 50,
+        destinationType: Camera.DestinationType.FILE_URI,
+        sourceType: Camera.PictureSourceType.CAMERA
+      };
+
+      console.log('Capturing photo with options: ' + options);
+      // Take picture using device camera and retrieve image as base64-encoded string
+      navigator.camera.getPicture(cameraSuccess, cameraError, options);
     };
 
     /*self.getPicture = function () {
@@ -25,32 +54,6 @@ window.report = (function(report) {
         alert('after camera call');
         console.log('after camera call');
     };*/
-
-    // Called when a photo is successfully retrieved
-    var onPhotoDataSuccess = function (imageData) {
-      alert('on succcess');
-      // Uncomment to view the base64 encoded image data
-      /*console.log(imageData);
-
-      // Get image handle
-      var firstImage = document.getElementById('photo-preview');
-
-      alert('image ' + firstImage);
-
-
-      // Unhide image elements
-      //smallImage.style.display = 'block';
-
-      // Show the captured photo
-      // The inline CSS rules are used to resize the image
-      //
-      firstImage.src = "data:image/jpeg;base64," + imageData;*/
-    };
-
-    var onFail = function (message) {
-      //alert('Failed because: ' + message);
-      alert('on fail');
-    };
 
     return self;
 })(window.report || {});
