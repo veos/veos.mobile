1657e8833d9dcbde3ff5da3b70de46d9ebc3bc59
Matt Zukowski <matt@zukowski.ca>
2012-05-05 19:49:09 -0400

    added generic 'alert' method for showing alerts on phonegap and regular browsers

--- js/veos.js
+++ js/veos.js
@@ -3,6 +3,14 @@
 window.veos = (function(veos) {
     var self = veos;           
 
+    self.alert = function (msg, title) {
+        if (navigator === undefined || navigator.notification === undefined) {
+            alert(msg);
+        } else {
+            navigator.notification.alert(msg, null, title);
+        }
+    }
+
     return self;
 })(window.veos || {});
 
