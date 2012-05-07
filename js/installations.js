91b7886a4f837ee0ccfc2901ca5ab85350f1bc4d
Armin Krauss <armin.krauss@utoronto.ca>
2012-05-06 20:48:36 -0400

    Fixed a lot of JSLint problems

--- js/installations.js
+++ js/installations.js
@@ -7,23 +7,21 @@ loopfunc: true, bitwise: true, curly: true, indent: 2, maxerr:50, white:false */
 window.installations = (function (installations) {
   var self = installations;
 
-  MAX_DISTANCE_FROM_CURRENT_LOCATION = 2;
+  MAX_DISTANCE_FROM_CURRENT_LOCATION = 10;
 
   self.init = function() {
     var r = new veos.model.Reports(); // creates the "reports" collection proxy object
 
     r.on('reset', function(collection) {
       navigator.geolocation.getCurrentPosition(function(currentLocation) {
-          var collectionFilter = function(report) {
-            return isCloseBy(report, currentLocation);
-          }
-          var filteredReports = collection.filter(collectionFilter);
+        var collectionFilter = function(report) {
+          return isCloseBy(report, currentLocation);
+        };
+        var filteredReports = collection.filter(collectionFilter);
 
-          createPointsGrid(filteredReports, currentLocation);
+        createPointsGrid(filteredReports, currentLocation);
 
-        },
-        geolocationFailureHandler
-      )
+      },geolocationFailureHandler);
     });
 
     // @triggers reset on the collection
@@ -35,10 +33,15 @@ window.installations = (function (installations) {
       // fills the installation-page.html grid (although as of now they points, not installations)
       var installationGrid = jQuery('#installations-page .ui-grid-a');
       jQuery('#installation-page .ui-grid-a').empty(); // clear out previous grids. May not be necessary
+      var buttonText = '';
 
       // creates the HTML for the jQuery button to be filled with returned content. Why are you so ugly jQuery?
-      var buttonText = report.get('owner_name') + " - " + report.get('sign') + "<br/>" + report.get('loc_description_from_google');
-      var divA = jQuery('<div class="ui-block-a">')
+      if (report.get('sign')) {
+        buttonText =  "Sign @ " + report.get('owner_name') + "<br/>" + report.get('loc_description_from_google');
+      } else if (report.get('camera')) {
+        buttonText = "Camera @ " + report.get('owner_name') + "<br/>" + report.get('loc_description_from_google');
+      }
+      var divA = jQuery('<div class="ui-block-a">');
       var installationOuterButton = jQuery('<a data-role="button" data-transition="fade" href="#point-details-page" data-icon="arrow-r" data-iconpos="left" data-theme="c" class="ui-btn ui-btn-icon-left ui-btn-corner-all ui-shadow ui-btn-up-c" />');
       var installationInnerButton = jQuery('<span class="ui-btn-inner ui-btn-corner-all">' + buttonText + '</span>');
       var installationButtonIcon = jQuery('<span class="ui-icon ui-icon-arrow-r ui-icon-shadow" />');
@@ -70,7 +73,7 @@ window.installations = (function (installations) {
 
 	
   function populatePointDetailsContent(report) {
-  	jQuery('#point-details-page .installation-title').text('Eaton Centre');
+    jQuery('#point-details-page .installation-title').text('Eaton Centre');
 
     // TODO: replace with Matt's stuff
 /*    var photoThumbnail = jQuery('<img class="photo-thumbnail" />');
@@ -78,33 +81,33 @@ window.installations = (function (installations) {
     var photoContainer = jQuery('#point-details-page .photo-thumbnail-container');
     photoContainer.append(photoThumbnail); */ 
 
-  	if (report.attributes.camera) {
-  		jQuery('#point-details-page .point-type').text('Camera');
-  		jQuery('#point-details-page .point-title-1').text('Type of space surveilled: ');
-  		jQuery('#point-details-page .point-content-1').text('parking lot');
-  		jQuery('#point-details-page .point-title-2').text('Camera\'s location: ');
-  		jQuery('#point-details-page .point-content-2').text('Accessible private space');
-  		jQuery('#point-details-page .point-title-3').text('Camera type: ');
-  		jQuery('#point-details-page .point-content-3').text('bullet');
-  	} else if (report.attributes.sign) {
-  		jQuery('#point-details-page .point-type').text('Sign');
-  		jQuery('#point-details-page .point-title-1').text('Visibility: ');
-  		jQuery('#point-details-page .point-content-1').text('Obscure/High');
-  		jQuery('#point-details-page .point-title-2').text('Stated Purpose: ');
-  		jQuery('#point-details-page .point-content-2').text('Public Safety');
-  		jQuery('#point-details-page .point-title-3').text('Stated Properties: ');
-  		jQuery('#point-details-page .point-content-3').text('Live monitoring');
-  		jQuery('#point-details-page .point-title-4').text('Text of Sign: ');
-  		jQuery('#point-details-page .point-content-4').text('loridium loripus ipsorino this could be a *lot* of text');
-  		jQuery('#point-details-page .point-content-4').append(jQuery('<br />'))
-  	} else {
-  		console.log ('neither a camera or a sign');
-  	}
+    if (report.attributes.camera) {
+      jQuery('#point-details-page .point-type').text('Camera');
+      jQuery('#point-details-page .point-title-1').text('Type of space surveilled: ');
+      jQuery('#point-details-page .point-content-1').text('parking lot');
+      jQuery('#point-details-page .point-title-2').text('Camera\'s location: ');
+      jQuery('#point-details-page .point-content-2').text('Accessible private space');
+      jQuery('#point-details-page .point-title-3').text('Camera type: ');
+      jQuery('#point-details-page .point-content-3').text('bullet');
+    } else if (report.attributes.sign) {
+      jQuery('#point-details-page .point-type').text('Sign');
+      jQuery('#point-details-page .point-title-1').text('Visibility: ');
+      jQuery('#point-details-page .point-content-1').text('Obscure/High');
+      jQuery('#point-details-page .point-title-2').text('Stated Purpose: ');
+      jQuery('#point-details-page .point-content-2').text('Public Safety');
+      jQuery('#point-details-page .point-title-3').text('Stated Properties: ');
+      jQuery('#point-details-page .point-content-3').text('Live monitoring');
+      jQuery('#point-details-page .point-title-4').text('Text of Sign: ');
+      jQuery('#point-details-page .point-content-4').text('loridium loripus ipsorino this could be a *lot* of text');
+      jQuery('#point-details-page .point-content-4').append(jQuery('<br />'));
+    } else {
+      console.log ('neither a camera or a sign');
+    }
   }
 
   function createPointDetailsMap(report) {
     var latLng = veos.map.retrieveLatLng(report);
-  	// note: higher zoom level
+    // note: higher zoom level
     var staticMapCriteria = "http://maps.googleapis.com/maps/api/staticmap?zoom=17&size=150x150&scale=2&sensor=true&center=" + latLng.lat + "," + latLng.lng;
     staticMapCriteria += "&markers=size:small%7C" + latLng.lat + ',' + latLng.lng;
     
@@ -114,9 +117,9 @@ window.installations = (function (installations) {
     thumbnailContainer.append(mapThumbnail);	
   }
 
-	function geolocationFailureHandler (errorMessage) {
-		alert('There was a problem determining your location due to: ' + error.message);
-	}
+  function geolocationFailureHandler (errorMessage) {
+    alert('There was a problem determining your location due to: ' + error.message);
+  }
 
   function isCloseBy(report, currentLocation) {
     var latLng = veos.map.retrieveLatLng(report);
@@ -129,29 +132,29 @@ window.installations = (function (installations) {
   }
 
   function distanceBetweenPoints(lat1, lng1, lat2, lng2) {
-		if (typeof(Number.prototype.toRad) === "undefined") {
-  		Number.prototype.toRad = function() {
-		    return this * Math.PI / 180;
-		  }
-		}
+    if (typeof(Number.prototype.toRad) === "undefined") {
+      Number.prototype.toRad = function() {
+        return this * Math.PI / 180;
+      };
+    }
 
-		lat1 = parseFloat(lat1);
-		lng1 = parseFloat(lng1);
-		lat2 = parseFloat(lat2);
-		lng2 = parseFloat(lng2);
+    lat1 = parseFloat(lat1);
+    lng1 = parseFloat(lng1);
+    lat2 = parseFloat(lat2);
+    lng2 = parseFloat(lng2);
 
-		var R = 6371; // km
-		var dLat = (lat2-lat1).toRad();
-		var dLng = (lng2-lng1).toRad();
-		lat1 = lat1.toRad();
-		lat2 = lat2.toRad();
+    var R = 6371; // km
+    var dLat = (lat2-lat1).toRad();
+    var dLng = (lng2-lng1).toRad();
+    lat1 = lat1.toRad();
+    lat2 = lat2.toRad();
 
-		var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-		        Math.sin(dLng/2) * Math.sin(dLng/2) * Math.cos(lat1) * Math.cos(lat2); 
-		var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-		d = R * c;
-		return d;
-	}
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.sin(dLng/2) * Math.sin(dLng/2) * Math.cos(lat1) * Math.cos(lat2); 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    d = R * c;
+    return d;
+  }
 
   return self;
 })(window.installations || {});
