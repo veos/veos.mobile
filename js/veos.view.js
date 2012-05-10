9bf1e9683d02222f9a6fe0f01cffb959f60e86be
Matt Zukowski <matt@zukowski.ca>
2012-05-10 10:44:11 -0400

    Owner name first in map bubbles

--- js/veos.view.js
+++ js/veos.view.js
@@ -267,9 +267,9 @@
 
                 // creates the HTML for the jQuery button to be filled with returned content. Why are you so ugly jQuery?
                 if (report.get('sign')) {
-                    buttonText =  "Sign @ " + report.get('owner_name') + "<br/>" + report.getLocDescription();
+                    buttonText =  report.get('owner_name') + " - Sign " +  + "<br/>" + report.getLocDescription();
                 } else if (report.get('camera')) {
-                    buttonText = "Camera @ " + report.get('owner_name') + "<br/>" + report.getLocDescription();
+                    buttonText = report.get('owner_name') + " - Camera " + "<br/>" + report.getLocDescription();
                 }
                 
                 var divA = jQuery('<div class="ui-block-a">');
