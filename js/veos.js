ea071c2434b9cdce6392cadee0733e1045426834
colinmccann <colindmccann@gmail.com>
2012-09-05 12:46:50 -0400

    refining map now centers over user_defined location when it exists

--- js/veos.js
+++ js/veos.js
@@ -94,6 +94,8 @@ window.veos = (function(veos) {
               var initLoc = veos.map.convertGeolocToGmapLatLng(veos.lastLoc);
               self.currentReport.set('loc_lng_from_gps', initLoc.lng());
               self.currentReport.set('loc_lat_from_gps', initLoc.lat());
+            } else {
+              // if we're coming straight from the splash page, we need to locate the user. I think this is the place to do it (bug 33)
             }
           }
 
@@ -126,9 +128,25 @@ window.veos = (function(veos) {
           return;
         }
 
-        var refinerMap = new veos.map.Map('#refine-location-canvas', veos.lastLoc);
+        var refinerMap;
+        var refinerLoc;
 
-        refinerMap.addReportRefinerMarker(self.reportForm.model, veos.lastLoc);
+        // if the user has made a change to the address bar, use that location
+        if (veos.currentReport.get('loc_lat_from_user') && veos.currentReport.get('loc_lng_from_user')) {
+          var refinerLoc = new google.maps.LatLng(veos.currentReport.get('loc_lat_from_user'), veos.currentReport.get('loc_lng_from_user'));
+          refinerMap = new veos.map.Map('#refine-location-canvas', refinerLoc);
+        }
+        // default case - user has not made any changes to location yet
+        else if (veos.lastLoc) {
+          refinerLoc = veos.lastLoc;
+          refinerMap = new veos.map.Map('#refine-location-canvas', refinerLoc);
+        }
+        // should never occur
+        else {
+          console.log("Cannot refine location because there is no lat/lng");
+        }
+
+        refinerMap.addReportRefinerMarker(self.reportForm.model, refinerLoc);
       })
 
     /** installations-list.html (installations-list-page) **/
